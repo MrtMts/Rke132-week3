191fca3fd9ad70e83eb6ac2376652e1934a7d4e8
MrtMts <mrmetsallik@gmail.com>
2024-05-19 18:16:56 +0300

    add week3 code

---
+++ hinded/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine("Mis hinde kasutaja sai:");
+
+string grade = Console.ReadLine() ;
+
+if (grade == "a")
+
+{
+
+    Console.WriteLine("Suurepärane!");
+
+}
+
+else if (grade == "b")
+
+{
+
+    Console.WriteLine("Väga hea!");
+
+}
+
+else if (grade == "c")
+
+{
+
+    Console.WriteLine("Hea!");
+
+}
+
+else if (grade == "d")
+
+{
+
+    Console.WriteLine("Rahuldav!");
+
+}
+
+else if (grade == "e")
+
+{
+
+    Console.WriteLine("Kasin!");
+
+}
+
+else if (grade == "f")
+
+{
+
+    Console.WriteLine("Puudulik!");
+
+}
+
+else
+
+{
+
+    Console.WriteLine("Vale väärtus!");
+
+}
---
+++ hindedSwitch/Program.cs
@@ -0,0 +1,29 @@
+﻿
+  
+        Console.WriteLine("Sisesta oma hinne: "); 
+        string grade = Console.ReadLine().ToUpper(); 
+
+        switch (grade)
+        {
+            case "A":
+                Console.WriteLine("Suurepärane!"); 
+                break;
+            case "B":
+                Console.WriteLine("Väga hea!"); 
+                break;
+            case "C":
+                Console.WriteLine("Hea!"); 
+                break;
+            case "D":
+                Console.WriteLine("Rahuldav"); 
+                break;
+            case "E":
+                Console.WriteLine("Kasin"); 
+                break;
+            case "F":
+                Console.WriteLine("Puudulik"); 
+                break;
+            default:
+                Console.WriteLine("vale väärtus"); 
+                break;
+        }
